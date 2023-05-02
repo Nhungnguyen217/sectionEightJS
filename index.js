f2901dcf01de7698a62da8ce52f3c7ea15cd92eb
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-05-02 22:33:41 +0700

    change

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const inputFieldEl = document.getElementById("input-field")
+const btnAdd = document.getElementById("add-button")
+btnAdd.addEventListener("click", function(){
+    let text = inputFieldEl.value 
+    console.log(text)
+})

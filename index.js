a67c6adb4ea4b88e84c6a98406c7c2d28814711c
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-05-04 18:15:19 +0700

    5-setting firebase url

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://catscrimbamobileapp-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
 
 const inputFieldEl = document.getElementById("input-field")
 const btnAdd = document.getElementById("add-button")
